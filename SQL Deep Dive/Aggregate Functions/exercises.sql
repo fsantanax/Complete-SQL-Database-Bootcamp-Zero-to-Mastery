f207c4688aedf37551e4ecee53dc28eaed7a3077
fsantanax <78623388+fsantanax@users.noreply.github.com>
2022-04-15 10:43:00 -0400

    AggregateFunctions.sql
    exercises done by fsantanax from Complete-SQL-Database-Bootcamp-Zero-to-Mastery

--- SQL Deep Dive/Aggregate Functions/exercises.sql
+++ SQL Deep Dive/Aggregate Functions/exercises.sql
@@ -8,11 +8,15 @@
 
 -- Question 1: What is the average salary for the company?
 -- Table: Salaries
-
+SELECT avg(salary) 
+FROM salaries;
+---- 63810.744836143706
 
 -- Question 2: What year was the youngest person born in the company?
 -- Table: employees
-
+SELECT max(birth_date) 
+FROM employees;
+--- 1965-02-01
 
 -- BEFORE YOU START
 /*
@@ -23,7 +27,9 @@
 
 -- Question 1: How many towns are there in france?
 -- Table: Towns
-
+SELECT count(id) 
+FROM towns;
+--- 36684 
 
 -- BEFORE YOU START
 /*
@@ -34,11 +40,21 @@
 
 -- Question 1: How many official languages are there?
 -- Table: countrylanguage
+SELECT count(countrycode) 
+FROM countrylanguage
+WHERE isofficial = TRUE;
+--- 238
 
 -- Question 2: What is the average life expectancy in the world?
 -- Table: country
+SELECT avg(lifeexpectancy)
+FROM country;
+--- 66.486036
 
 -- Question 3: What is the average population for cities in the netherlands?
 -- Table: city
-
+SELECT avg(population) 
+FROM city 
+WHERE countrycode = 'NLD'
+--- 185001.75
 
