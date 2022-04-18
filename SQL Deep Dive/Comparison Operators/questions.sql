2541820254b1ba757c01592c6ffdacbfa985d62b
fsantanax <78623388+fsantanax@users.noreply.github.com>
2022-04-17 21:56:40 -0400

    ComparisonOperators.sql
    Done by fsantanax

--- SQL Deep Dive/Comparison Operators/questions.sql
+++ SQL Deep Dive/Comparison Operators/questions.sql
@@ -1,21 +1,27 @@
 -- How many female customers do we have from the state of Oregon (OR)?
 /*
-* Write your query here
+SELECT count(customerid) FROM customers
+where gender = 'F' AND state = 'OR'
+---- Result = 106
 */
 
 -- Who over the age of 44 has an income of 100 000 or more? (excluding 44)
 /*
-* Write your query here
+SELECT firstname, lastname, age, income from customers
+Where age > '44' and income >= '100000'
 */
 
 -- Who between the ages of 30 and 50 has an income less than 50 000?
 -- (include 30 and 50 in the results)
 
 /*
-* Write your query here
+SELECT firstname, lastname, age, income from customers
+Where age >= '30' AND age <='50' and income < '50000'
 */
 
 -- What is the average income between the ages of 20 and 50? (Excluding 20 and 50)
 /*
-* Write your query here
+SELECT avg(income) from customers
+Where age > '20' AND age < '50' 
+-- Result = 59409.926240780098
 */
